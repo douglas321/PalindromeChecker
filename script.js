54b9fc0baa64a22c16f10e6c404a270deb9dfc83
douglas321 <sperrycarter@gmail.com>
2024-11-26 14:48:36 -0700

    created results display

---
+++ script.js
@@ -0,0 +1,23 @@
+function getInputText(){
+    const textElement = document.getElementById("text-input");
+    const textValue= textElement.value;
+    return textValue;
+}
+
+function checkPalindrome(){
+    const text = getInputText();
+
+    if(!text){
+        alert("Please input a value");
+    }
+    
+    const cleanedText = text.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+    const reversedText = cleanedText.split('').reverse().join('');
+
+    const resultElement = document.getElementById("result"); 
+    if (cleanedText === reversedText){
+        resultElement.textContent = `${text} is a palindrome.`
+    }   else{
+        resultElement.textContent = `${text} is not a palindrome.`
+    }
+}
